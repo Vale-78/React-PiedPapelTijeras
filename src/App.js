bd3bab7a43149f929123026a317a36e46c8a1886
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-09-13 13:13:47 -0300

    Reinicio de mi proyecto.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React from "react";
 import { Saludo } from "./componentes/Saludo";
 //Iniciar juego es para iniciar y reininiar juego
-import { Juego } from "./componentes/Juego";
+import { Botones } from "./componentes/Botones";
 // import { Resultados } from "./componentes/Resultados";
 import { InfoTitulo } from "./componentes/InfoTitulo";
 import { useState } from "react";
@@ -11,24 +11,36 @@ import { IniciarJuego } from "./componentes/IniciarJuego";
 
 // https://github.com/uqbar-project/eg-hola-mundo-react/tree/master  ejemplo
 // https://es.linkedin.com/learning/domina-react/piedra-papel-o-tijera
+
 function App() {
-  const opciones = ["piedra", "papel", "tijeras"];
   const [values, setValues] = useState("");
   const [desbloquearJuego, setDesbloquearJuego] = useState(0);
-  const [compuSelec, setCompuSelec] = useState("");
-  const [jugadorSelec, setJugadorSelec] = useState("");
-  const [puntosCompu, setPuntosCompu] = useState(0);
-  const [puntosJugador, setPuntosJugador] = useState(0);
-  const [resultado, setResultado] = useState("");
-  const [mostrarResultado, setMostrarResultado] = useState(false);
+  // const [compuSelec, setCompuSelec] = useState("");
+  // const [jugadorSelec, setJugadorSelec] = useState("");
+  // const [puntosCompu, setPuntosCompu] = useState(0);
+  // const [puntosJugador, setPuntosJugador] = useState(0);
+  // const [resultado, setResultado] = useState("");
+  // const [mostrarResultado, setMostrarResultado] = useState(false);
 
+  // const agregoEmoji = (opcion) => {
+  //   switch (opcion) {
+  //     case "piedra":
+  //       return "🪨";
+  //     case "papel":
+  //       return "🧻";
+  //     case "tijeras":
+  //       return "✂️";
+  //     default:
+  //       break;
+  //   }
+  // };
   return (
     <div className="App">
       <div className="info-container">
         <InfoTitulo />
       </div>
 
-      {/* <section id="saludoContainer" className='usuarioNameContainer'> */}
+   
       <Saludo
         nombre={values}
         addNombre={setValues}
@@ -36,71 +48,35 @@ function App() {
       />
 
       <div>
-        {/* <section id="inicarJuego"> */}
-        <IniciarJuego desbloquearJuego={setDesbloquearJuego} />
-        {desbloquearJuego === 1 ? (
-          <Juego
-            opciones={opciones}
-            jugadorSelec={jugadorSelec}
-            setJugadorSelec={setJugadorSelec}
-            mostrarResultado={mostrarResultado}
-            setMostrarResultado={setMostrarResultado}
-            compuSelec={compuSelec}
-            setCompuSelec={setCompuSelec}
-            resultado={resultado}
-            setResultado={setResultado}
-            puntosCompu={puntosCompu}
-            setPuntosCompu={setPuntosCompu}
-            puntosJugador={puntosJugador}
-            setPuntosJugador={setPuntosJugador}
-          />
-        ) : (
-          <></>
-        )}
 
-        {/* <Resultados
+        <IniciarJuego desbloquearJuego={setDesbloquearJuego} />
+
+        {desbloquearJuego === 1 ? <Botones /> : <></>}
+       </div>
+        {/* <div>
+          <h2>Puntajes</h2>
+          <p>Compu: {puntosCompu}</p>
+          <p>Tu puntaje: {puntosJugador}</p>
+        </div>
+      </div>
+
+      {mostrarResultado && (
+        <div>
+          <div>Compu : {agregoEmoji(compuSelec)}</div>
+          <div>Tu eleccionCompu: {agregoEmoji(jugadorSelec)}</div>
+          <div>
+            <h4>Resultado Final</h4>
+            <h4>{resultado}</h4>
+          </div>
+        </div>
+      )} */}
+
+      {/* <Resultados
          
           nombre={values}
           addNombre={setValues}
          
         /> */}
-      </div>
-
-      {/* <section id="seleccionar-jugada"> */}
-      <div></div>
-
-      <div className="botones-container">
-        <div className="botones">
-          <div className="efecto">
-            <button id="Piedra">
-              <img id="piedra" src="./assest/piedra.png" alt="" />
-            </button>
-            <button id="Papel">
-              <img id="papel" src="./assest/papel.png" alt="" />
-            </button>
-            <button id="Tijeras">
-              <img id="tijeras" src="./assest/tijeras.png" alt="" />
-            </button>
-          </div>
-          <div className="parrafos">
-            <p id="eleccionJugador" className="p1" />
-            <p id="eleccionCompu" className="p2" />
-            <p id="resultadoRonda" className="p3" />
-            <p id="numeroGanaUsuario" className="p4" />
-            <p id="numeroGanaPC" className="p5" />
-            <p id="resultFinal" className="p6" />
-          </div>
-        </div>
-      </div>
-      {/* </section> */}
-
-      {/* <section id="reiniciarJuego"> */}
-      <div className="boton-reiniciar">
-        <button id="resetButton" className="botonReiniciar">
-          Reiniciar Juego
-        </button>
-      </div>
-      {/* </section> */}
     </div>
   );
 }
---
+++ src/componentes/Botones.jsx
@@ -0,0 +1,11 @@
+// const ResultadosParciales = ({ jugadaElegida, setJugadaElegida }) => {
+//   const increment = () => {
+//     setJugadaElegida(jugadaElegida + 1);
+//   };
+//   return (
+//     <div>
+//       <button onClick={increment}>{jugadaElegida}</button>
+//     </div>
+//   );
+// };
+// export {ResultadosParciales};
